6cb8fec91202482325ac3c8d5b8c8128c2a3c717
GustavoDSCruz <gustavodiasdsc@gmail.com>
2022-09-28 23:49:03 -0300

    vamos la

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,7 +1,9 @@
-export default function tempo(request, response){
+function tempo(request, response){
     const dynamicDate = new Data();
 
     response.json({
         date: dynamicDate.toGMTString()
-    })
+    });
 }
+
+export default tempo;
